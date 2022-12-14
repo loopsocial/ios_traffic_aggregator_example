0d3f924bcbabf6ce9875898cf0e55859068a3bf4
Junard Lebajan <junard.lebajan@gmail.com>
2022-12-14 14:15:25 -0800

    Update binary zip url.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrafficAggregatorSDK",
-            url: "https://github.com/loopsocial/ios_traffic_aggregator_example/releases/download/0.2.0/TrafficAggregatorSDK.zip",
+            url: "https://github.com/loopsocial/ios_traffic_aggregator_example/releases/download/v0.2.0/TrafficAggregatorSDK.zip",
             checksum: "2518bd4543c01215f84fc7010e134d5a814abe03a75806e12862a18281b8992d"
         )
     ]
