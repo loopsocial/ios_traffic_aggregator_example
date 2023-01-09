1a3c418ceb904ca6fd4649929163b3ed971785db
Junard Lebajan <junard.lebajan@gmail.com>
2023-01-09 14:16:23 -0500

    Updates xcframework: Adds navigation control bar under web view.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.0-beta.2]
+
+### Changed
+
+  - Added navigation control bar under the web view.
+
 ## [1.0.0.beta.1]
 
 ### Changed
--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrafficAggregatorSDK",
-            url: "https://github.com/loopsocial/ios_traffic_aggregator_example/releases/download/v1.0.0-beta.1/TrafficAggregatorSDK.zip",
-            checksum: "004ea36b2a1829c96fcfcd8f154911d70113cf94ecf63407000fa7643859e944"
+            url: "https://github.com/loopsocial/ios_traffic_aggregator_example/releases/download/v1.0.0-beta.2/TrafficAggregatorSDK.zip",
+            checksum: "0493bda9fe49ea5e305c2e87cc93434cdf67765e2ce7a5274a8b95a6d293d553"
         )
     ]
 )
